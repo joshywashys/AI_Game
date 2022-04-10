59112bd81d63497c4fb9d5a95ea71bfb24ff2b2c
joshywashys <joshuastrutt1@gmail.com>
2022-04-09 21:54:34 -0400

    enemy seek

--- AIGame/Assets/Scripts/Enemy.cs
+++ AIGame/Assets/Scripts/Enemy.cs
@@ -4,18 +4,65 @@ using UnityEngine;
 
 public class Enemy : EntityBase
 {
-    float maxSpeed;
-    float maxAccel;
+    Rigidbody2D rb2d;
 
-    // Tracking player
+    #region Steering
+    public float maxSpeed = 8;
+    public float maxAccel = 1;
+    public float slowRadius = 1;
+    public float targetRadius = 1;
+    public float rotationSpeed = 1;
+    private Vector2 velocity;
+
+    struct steeringParams
+    {
+        public Vector2 linear;
+        public float angular;
+    }
+    steeringParams steer;
+
+    steeringParams GetSteering()
+    {
+        steeringParams result = new steeringParams();
+        result.linear = targetPos - (Vector2)transform.position;
+        result.linear.Normalize();
+        result.linear *= maxAccel;
+
+        result.angular = 0;
+
+        return result;
+    }
+
+
+    private Vector2 targetPos;
     public Transform target;
+    public void GetPlayer()
+    {
+        Transform player = FindObjectOfType<PlayerController>().transform;
+        if (player != null)
+        {
+            target = player;
+        }
+        else
+        {
+            target = transform;
+        }
+    }
 
 
+    #endregion
+
+    #region Attacking
     // Attacking player
     public WeaponBase weapon;
     //public float attackRange; //(get from weapons)
-    
-    
+
+
+    private void Attack()
+    {
+        //weapon.Attack();
+    }
+    #endregion
 
     public override void Damage(float amount)
     {
@@ -28,9 +75,32 @@ public class Enemy : EntityBase
         Destroy(gameObject);
     }
 
-    private void Attack()
+    #region Monobehaviour Functions
+
+    public void Start()
     {
-        //uses weapon
+        // Initialize Variables
+        if (target == null) { GetPlayer(); } //ignore player if target set in editor
+        velocity = Vector2.zero;
+        rb2d = GetComponent<Rigidbody2D>();
+        targetRadius = weapon.;
+        slowRadius = maxSpeed * 2 + targetRadius;
     }
+
+    public void FixedUpdate()
+    {
+        // Update Variables
+        targetPos = target.position;
+        steeringParams newSteer = GetSteering();
+
+        velocity += newSteer.linear * Time.fixedDeltaTime;
+        //rotation = newSteer.angular;
+
+        // Steer
+        //transform.position += (Vector3)velocity * Time.fixedDeltaTime;
+        rb2d.MovePosition(rb2d.position + velocity * Time.fixedDeltaTime);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(target.position, Vector3.up), Time.deltaTime * rotationSpeed);  //rotation * Time.deltaTime;
+    }
+    #endregion
 
 }
